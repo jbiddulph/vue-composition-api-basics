add895516ca197fbb2f25fc6e574db4626997207
jbiddulph <john.mbiddulph@gmail.com>
2023-10-07 14:24:47 +0100

    slots and props

---
+++ src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <teleport to=".modals-container">
+      <div
+        class="modal"
+      >
+        <h2>{{ title }}</h2>
+        <slot />
+        <button>
+          Hide modal
+        </button>
+      </div>
+    </teleport>
+</template>
+
+<script setup>
+/* 
+Props
+*/
+const props = defineProps({
+  title: {
+    type: PerformanceServerTiming,
+    default: 'No title specified'
+  }
+})
+</script>
+
+<style>
+.modal {
+  background: beige;
+  padding: 10px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,18 +2,9 @@
   <div class="modals">
     <h1>modals</h1>
     <button @click="showModal = true">Show button</button>
-    <teleport to=".modals-container">
-      <div
-        v-if="showModal"
-        class="modal"
-      >
-        <h2>this is the modal</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias optio laborum odit consequuntur reiciendis recusandae. Dolores quisquam nisi cumque laborum adipisci modi, ratione aliquid perferendis dicta incidunt ipsam? Quidem, provident?</p>
-        <button @click="showModal = false">
-          Hide modal
-        </button>
-      </div>
-    </teleport>
+    <Modal v-if="showModal" title="My modal title via prop">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias optio laborum odit consequuntur reiciendis recusandae. Dolores quisquam nisi cumque laborum adipisci modi, ratione aliquid perferendis dicta incidunt ipsam? Quidem, provident?</p>
+    </Modal>
   </div>
 </template>
 
@@ -22,22 +13,10 @@
 imports
 */
 import { ref } from 'vue'
+import Modal from '@/components/Modal.vue'
 /*
 modals
 */
 
 const showModal = ref(false)
 </script>
-
-<style>
-.modal {
-  background: beige;
-  padding: 10px;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 1;
-}
-</style>
