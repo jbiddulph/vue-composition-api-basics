32cd84c6d06a9add8b247c5203b7b657b2dd2985
jbiddulph <john.mbiddulph@gmail.com>
2023-10-07 10:52:28 +0100

    the basics

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <textarea v-autofocus />
+  </div>
+</template>
+
+<script setup>
+import { vAutofocus } from '@/directives/vAutofocus'
+</script>
