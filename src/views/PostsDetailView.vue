79216e7e809215814557438c31aa025420aef1fc
jbiddulph <john.mbiddulph@gmail.com>
2023-10-07 11:40:27 +0100

    useRouter and useRoute

---
+++ src/views/PostsDetailView.vue
@@ -0,0 +1,39 @@
+
+<template>
+  <div class="posts-detail">
+    <h1>This is a post page</h1>
+    <p>Display the content of ID {{ $route.params.id }}</p>
+    <div>
+      <button @click="showPostId">Show post ID</button>
+    </div>
+    <div>
+      <button @click="goHomeIn3Secs">Go home in 3 seconds</button>
+    </div>
+    <div>
+      <button @click="goToFirstPost">Go to first post</button>
+    </div>
+    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+const route = useRoute()
+const router = useRouter()
+const showPostId = () => {
+alert(`The ID is ${route.params.id}`)
+}
+const goHomeIn3Secs = () => {
+  setTimeout(() => {
+    router.push('/')
+  }, 3000)
+}
+const goToFirstPost = () => {
+  router.push({
+    name: 'postsDetail',
+    params: {
+      id: 'id1'
+    }
+  })
+}
+</script>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+    <ul>
+      <li>
+        <RouterLink to="/posts/id1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/posts/id2">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/posts/id3">Post 3</RouterLink>
+      </li>
+    </ul>
+    <textarea v-autofocus />
+  </div>
+</template>
+
+<script setup>
+import { vAutofocus } from '@/directives/vAutofocus'
+</script>
+
+<style scoped>
+ul {
+  margin-bottom: 30px;
+}
+</style>
